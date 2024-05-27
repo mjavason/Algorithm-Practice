83c3ecb1b194a916fd54ab5470429d7bdf1f1c20
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2024-05-27 19:06:06 +0100

    Add minMax test

---
+++ minmax.ts
@@ -0,0 +1,22 @@
+// Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+
+function miniMaxSum(arr: number[]): void {
+    // Write your code here
+    let minimum = 0;
+    let maximum = 0;
+    for(let i = 0; i < arr.length; i++){
+        let total = 0;
+        let arrayCopy = [...arr];
+       arrayCopy.splice(i, 1);
+       
+         for(let j = 0; j < arrayCopy.length; j++){
+        total= total + arrayCopy[j];
+        }
+        
+        if(minimum > total) minimum = total;    
+        if(total > maximum) maximum = total;
+        if(i == 0)  minimum = total;    
+    }
+    
+console.log(minimum, maximum)
+}
--- staircase.ts
+++ staircase.ts
@@ -1,3 +1,11 @@
+// Staircase detail
+// This is a staircase of size n = 4:
+//    #
+//   ##
+//  ###
+// ####
+// Its base and height are both equal to n. It is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+
 function staircase(n: number): void {
   // Write your code here
 
