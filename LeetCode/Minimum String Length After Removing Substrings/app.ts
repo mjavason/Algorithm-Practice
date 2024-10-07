a42e8a1039740f9effeabffe39f99eb018b5502a
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2024-10-07 10:18:21 +0100

    Update

--- LeetCode/Minimum String Length After Removing Substrings/app.ts
+++ LeetCode/Minimum String Length After Removing Substrings/app.ts
@@ -1,15 +1,8 @@
-const substring1 = 'AB';
-const substring2 = 'CD';
-
 function minLength(s: string) {
-  //start a infinite loop
-  for (let i = 1; i > 0; i++) {
-    if (!s.includes(substring1) && !s.includes(substring2)) return s.length;
-
-    if (s.includes(substring1)) s = s.replace(substring1, '');
-
-    if (s.includes(substring2)) s = s.replace(substring2, '');
+  while (s.includes('AB') || s.includes('CD')) {
+    if (s.includes('AB')) s = s.replace('AB', '');
+    if (s.includes('CD')) s = s.replace('CD', '');
   }
-}
 
-console.log(minLength('FCABD'));
+  return s.length;
+}
