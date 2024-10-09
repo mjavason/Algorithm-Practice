c95b5cae3b59d9801a2b7080b3f31a7430b57a3c
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2024-10-09 23:02:49 +0100

    Minor optimizations

--- LeetCode/Longest Palindromic String/app.ts
+++ LeetCode/Longest Palindromic String/app.ts
@@ -1,21 +1,20 @@
 function longestPalindrome(s: string): string {
-  if (s == s.split('').reverse().join('')) return s;
   let longestString = s[0];
+  let currentString;
 
   for (let i = 0; i < s.length; i++) {
-    let currentString = s[i];
+    currentString = s[i];
     for (let k = i + 1; k < s.length; k++) {
       currentString += s[k];
-      if (
-        s[i] == s[k] &&
-        currentString == currentString.split('').reverse().join('') &&
-        currentString.length > longestString.length
-      )
-        longestString = currentString;
+      if (s[i] == s[k] && currentString.length > longestString.length) {
+        if (currentString == currentString.split('').reverse().join(''))
+          longestString = currentString;
+      }
     }
+    if (longestString.length >= s.length - i + 1) return longestString;
   }
 
   return longestString;
 }
 
-console.log(longestPalindrome('abcacac'));
+console.log(longestPalindrome('acabbbbbbb'));
